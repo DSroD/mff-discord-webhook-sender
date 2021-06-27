b23767bbcad01192f56f91d3b754aaea01998787
DSroD <dsrod@seznam.cz>
2021-06-27 16:29:13 +0200

    fetch discord webhook

--- src/EmbedComponent.js
+++ src/EmbedComponent.js
@@ -3,21 +3,16 @@ import Form from 'react-bootstrap/Form';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 
-const EmbedComponent = ({embId, delFunc}) => {
-
-    const [authorName, setAuthorName] = useState('');
-    const [embedTitle, setEmbedTitle] = useState('');
-    const [embedDescription, setEmbedDescription] = useState('');
-
+const EmbedComponent = ({embId, delFunc, updateFunc}) => {
     return (
-        <Form.Group controlId={"mff-discord-webhook.Embed." + embId}>
+        <Form.Group>
         <div className="embed">
             <Button className="embed-close" variant="outline-dark" onClick={() => {delFunc(embId)}}>x</Button>
             <Card className="embed-card">
                 <Card.Body>
-                    <Form.Control onChange={(e) => setAuthorName(e.target.value)} className="msg-form-input" type="text" placeholder="Author name"/>
-                    <Form.Control onChange={(e) => setEmbedTitle(e.target.value)} className="msg-form-input" type="text" placeholder="Embed title"/>
-                    <Form.Control onChange={(e) => setEmbedDescription(e.target.value)} className="msg-form-input" type="text" placeholder="Embed description"/>
+                    <Form.Control onChange={(e) => updateFunc(embId, {authorName: e.target.value})} className="msg-form-input" type="text" placeholder="Author name"/>
+                    <Form.Control onChange={(e) => updateFunc(embId, {embedTitle: e.target.value})} className="msg-form-input" type="text" placeholder="Embed title"/>
+                    <Form.Control onChange={(e) => updateFunc(embId, {embedDescription: e.target.value})} className="msg-form-input" type="text" placeholder="Embed description"/>
                 </Card.Body>
             </Card>
         </div>
--- src/MessageComponent.js
+++ src/MessageComponent.js
@@ -8,27 +8,51 @@ class MessageComponent extends React.Component {
   constructor(props) {
     super(props);
     this.deleteEmbed = this.deleteEmbed.bind(this);
+    this.updateEmbed = this.updateEmbed.bind(this);
   }
 
   state = {
     embeds: [],
+    embed_data: [],
     nextId: 0,
     username: '',
     message: '',
-    avatarURL: ''
+    avatarURL: '',
+    webhook: localStorage.getItem('webhookURI') || ''
   }
 
   deleteEmbed = (id) => {
     this.setState({
-      embeds: this.state.embeds.filter(emb => emb.props.embId != id)
+      embeds: this.state.embeds.filter(emb => emb.props.embId != id),
+      embed_data: this.state.embed_data.filter(data => data.id != id)
     });
   }
 
   addEmbed = () => {
     this.setState({
-        embeds: [...this.state.embeds, <EmbedComponent embId={this.state.nextId} delFunc={this.deleteEmbed}/>],
+        embeds: [...this.state.embeds, <EmbedComponent embId={this.state.nextId} delFunc={this.deleteEmbed} updateFunc={this.updateEmbed}/>],
+        embed_data: [... this.state.embed_data, {id: this.state.nextId, authorName: '', embedTitle: '', embedDescription: ''}],
         nextId: this.state.nextId + 1
     });
+  }
+
+  updateEmbed = (id, val) => {
+    this.setState({
+     embed_data: this.state.embed_data.map((el) => {
+        if(el.id != id) {return el;}
+        return {
+          ...el,
+          ...val
+        }
+      })
+    })
+  }
+
+  setWebhook = (wh) => {
+    localStorage.setItem('webhookURI', wh);
+    this.setState({
+      webhook: wh
+    })
   }
 
   setAvatarURL = (target) => {
@@ -43,9 +67,41 @@ class MessageComponent extends React.Component {
     this.setState({avatarURL: url});
   }
 
+  sendWebhookRequest = (target) => {
+    var body = {
+      content: this.state.message,
+      username: this.state.username
+    }
+
+    if (this.state.avatarURL) {
+      body["avatar_url"] = this.state.avatarURL;
+    }
+
+    if(this.state.embeds.length > 0) {
+      body["embeds"] = [];
+      this.state.embed_data.forEach((el) => {
+        body.embeds.push({
+          author: {
+            name: el.authorName
+          },
+          title: el.embedTitle,
+          description: el.embedDescription
+        })
+      })
+    }
+
+    var requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(body)
+    }
+
+    fetch(this.state.webhook, requestOptions);
+  }
+
   render() {
     return(
-      <div>
+      <div className="message-form">
         <button onClick={(e) => this.setAvatarURL(e.target)} id="avatar" className="avatar-main" title="Set avatar URL"></button>
         <div>
           <Form.Group className="main-message" controlId="mff-discord-webhook.Message">
@@ -53,8 +109,10 @@ class MessageComponent extends React.Component {
             <Form.Control onChange={(e) => this.setState({message: e.target.value})} className="msg-form-input" as="textarea" rows={2} placeholder="Message (max 2000 characters)" />
           </Form.Group>
           {this.state.embeds}
-          <Button variant="primary" onClick={(e) => this.addEmbed()}>Add Embed</Button>
+          <Button className="msg-form-button" variant="primary" onClick={(e) => this.addEmbed()}>Add Embed</Button>
         </div>
+        <Button className="msg-form-button" variant="primary" onClick={(e) => this.sendWebhookRequest(e.target)}>Send Message</Button>
+        <Form.Control onChange={(e) => this.setWebhook(e.target.value)} className="msg-form-input" type="text" placeholder="Discord webhook" value={this.state.webhook}/>
       </div>
     )
   }
